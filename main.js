4d952dac2b1372d5a0c7d992acc404700f06e268
harryg12345 <132217767+harryg12345@users.noreply.github.com>
2024-01-22 18:37:59 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup() {
+    canvas = createCanvas(640,480)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+}
+function draw() {
+    circle(400, 70, 20)
+    circle(50, 70, 20)
+rect(20,10,20, 40)
+}
+
+ 
